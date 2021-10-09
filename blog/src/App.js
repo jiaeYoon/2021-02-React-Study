d36ea3a017b61acb6d5f43607a3a1f1700d46212
seohyun319 <tjguselly0319@gmail.com>
2021-10-10 05:12:40 +0900

    [1주차] REACT 시작하기 & JSX

---
+++ blog/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div style={{backgroundColor: "black", padding: "16px", color: "#61DAFB", fontSize: "50px"}}>
+          리액트 JSX 공부 인증 (김서현)
+        </div>
+    </div>
+  );
+}
+export default App;

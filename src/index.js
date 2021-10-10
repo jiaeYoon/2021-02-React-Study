6aa2e7edc6fb9b1935552d24adf96dce266db91d
jihye <asd36384@gmail.com>
2021-10-10 16:34:10 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+//import UseAxios from './react-hooks/UseAxios';
+//const {loading, error, data} = UseAxios({url: "https://cors-anywhere.herokuapp.com/https://yts.am/api/v2/list_movies.json"})
+//console.log(`loading: ${loading}, error: ${error}, data: ${data}`);
+ReactDOM.render(<App />, document.getElementById("root"));
